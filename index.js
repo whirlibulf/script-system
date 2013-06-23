5950caa5e3d7ab4665f34e70f80a822bee686865
whirlibulf <whirlibulf.engine@gmail.com>
2013-06-23 23:04:00 +0100

    Call init on scripts when game starts

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ function System() {
 System.prototype.init = function (engine) {
   console.log("Script system loaded");
   this.engine = engine;
+
+  this.engine.on("start", function () {
+      var entities, i, component;
+
+      entities = this.engine.getAll("script");
+      for (i = 0; i < entities.length; ++i) {
+        component = this.engine.get(entities[i], "script");
+        for (j = 0; j < component.scripts.length; ++j) {
+          component.scripts[j].init(this.engine, entities[i]);
+        }
+      }
+  });
 };
 
 System.prototype.update = function (dt) {
