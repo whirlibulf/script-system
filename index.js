fe84a0218d5566e3fdea3251f09c2751e386b6ce
whirlibulf <whirlibulf.engine@gmail.com>
2013-06-23 23:05:01 +0100

    Make sure script functions exist before calling

--- index.js
+++ index.js
@@ -2,32 +2,36 @@ function System() {
 }
 
 System.prototype.init = function (engine) {
-  console.log("Script system loaded");
-  this.engine = engine;
+    console.log("Script system loaded");
+    this.engine = engine;
 
-  this.engine.on("start", function () {
-      var entities, i, component;
+    this.engine.on("start", function () {
+        var entities, i, component;
 
-      entities = this.engine.getAll("script");
-      for (i = 0; i < entities.length; ++i) {
-        component = this.engine.get(entities[i], "script");
-        for (j = 0; j < component.scripts.length; ++j) {
-          component.scripts[j].init(this.engine, entities[i]);
+        entities = this.engine.getAll("script");
+        for (i = 0; i < entities.length; ++i) {
+            component = this.engine.get(entities[i], "script");
+            for (j = 0; j < component.scripts.length; ++j) {
+                if (component.scripts[j].init) {
+                    component.scripts[j].init(this.engine, entities[i]);
+                }
+            }
         }
-      }
-  });
+    });
 };
 
 System.prototype.update = function (dt) {
-  var entities, i, component;
+    var entities, i, component;
 
-  entities = this.engine.getAll("script");
-  for (i = 0; i < entities.length; ++i) {
-    component = this.engine.get(entities[i], "script");
-    for (j = 0; j < component.scripts.length; ++j) {
-      component.scripts[j].update(this.engine, entities[i], dt);
+    entities = this.engine.getAll("script");
+    for (i = 0; i < entities.length; ++i) {
+        component = this.engine.get(entities[i], "script");
+        for (j = 0; j < component.scripts.length; ++j) {
+            if (component.scripts[j].update) {
+                component.scripts[j].update(this.engine, entities[i], dt);
+            }
+        }
     }
-  }
 };
 
 module.exports = System;
